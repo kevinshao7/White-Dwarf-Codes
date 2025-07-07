d82636f8f1b54e85bcdf84ca55690ffbcca651c5
Kevin <shaoqihen@gmail.com>
2025-07-07 15:05:02 -0400

    first commit

---
+++ Annie/runradlev016.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+#PBS -S /bin/bash 
+#PBS -l select=2:ncpus=40:mpiprocs=40:ompthreads=1 
+#PBS -q s1 
+#PBS -N radlev
+#PBS -M amalone7@u.rochester.edu 
+#PBS -j oe 
+
+cd $PBS_O_WORKDIR 
+
+module load lammps/2Aug2023/b2
+mpirun -np 80 lmp_mpi -log log016.lammps -in radlev016.in
---
+++ forced/forced_base.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --partition=standard --time=00:30:00 --output=tmp.log
+#SBATCH --mail-type=END
+#SBATCH --mail-user=ks2120@cam.ac.uk
+#SBATCH -n 36
+#SBATCH -o log.txt
+module load openmpi/2.1.1/b1
+module load voro/0.4.6
+module load eigen/3.3.2
+module load latte/1.1.1
+module load lammps/23Jun2022/b1
+mpiexec -n 36 lmp_mpi -in forced_base.in
