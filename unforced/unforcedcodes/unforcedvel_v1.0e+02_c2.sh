c717ecce27fac09949234da67b193ad849438028
Kevin <shaoqihen@gmail.com>
2025-10-08 02:34:17 +0100

    save2

--- unforced/unforcedcodes/unforcedvel_v1.0e+02_c2.sh
+++ unforced/unforcedcodes/unforcedvel_v1.0e+02_c2.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --partition=standard --time=11:59:00 --exclude=bhc0032
+#SBATCH --partition=standard --time=2:59:00 
 #SBATCH --mail-type=END
 #SBATCH --mail-user=ks2120@cam.ac.uk
 #SBATCH -n 20
-#SBATCH -o unforcedslumlogvel_v1.0e+00_dt1.0e-04.txt
+#SBATCH -o unforcedslumlogvel_v1.0e+02_c2.txt
 module load openmpi/2.1.1/b1
 module load voro/0.4.6
 module load eigen/3.3.2
 module load latte/1.1.1
 module load lammps/23Jun2022/b1
-mpiexec -n 20 lmp_mpi -in unforcedvel_v1.0e+00_dt1.0e-04.in
+mpiexec -n 20 lmp_mpi -in unforcedvel_v1.0e+02_c2.in
