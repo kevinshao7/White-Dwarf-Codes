a67bfc9abf40a6fead6928b6b3dd58a91bf2abb0
Kevin <shaoqihen@gmail.com>
2026-08-04 18:05:18 +0100

    8/4b

---
+++ unforced/dais/unforcedvel_v1.0e+05_c2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH  --time 12:00:00 
+#SBATCH --mail-type=END
+#SBATCH --mail-user=ks2120@cam.ac.uk
+#SBATCH -n 30
+#SBATCH -o unforcedslumlogvel_v1.0e+05_c2.txt
+module load voro/0.4.6
+module load eigen/3.3.2
+module load latte/1.1.1
+mpiexec -n 30 /u/kshao/software/lammps-install/bin/lmp -in unforced_base.in
---
+++ unforced/unforcedcodes/unforcedvel_v1.6e+07_c2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --partition=standard --time 12:00:00 
+#SBATCH --mail-type=END
+#SBATCH --mail-user=ks2120@cam.ac.uk
+#SBATCH -n 30
+#SBATCH --exclude=bhc0020,bhc0021,bhc0025
+#SBATCH -o unforcedslumlogvel_v1.6e+07_c2.txt
+module load openmpi/2.1.1/b1
+module load voro/0.4.6
+module load eigen/3.3.2
+module load latte/1.1.1
+module load lammps/23Jun2022/b1
+mpiexec -n 30 lmp_mpi -in unforcedvel_v1.6e+07_c2.in
